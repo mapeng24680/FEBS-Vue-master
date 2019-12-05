f72eef049858109a3415dd987438652347882eea
ysb25536 <1031555163@qq.com>
2019-12-05 23:23:05 +0800

    增加试题页面

---
+++ frontend/src/views/administration/exam_mgr_new/popEdit.vue
@@ -0,0 +1,26 @@
+<template>
+    <a-modal
+      title="管理考试分类"
+      :visible="visible"
+      @ok="handleOk"
+      :confirmLoading="confirmLoading"
+      @cancel="visible=false"
+    >
+      <p></p>
+    </a-modal>
+</template>
+<script>
+export default {
+    data(){
+        return{
+        }
+    },
+    props:{
+        visible:{
+        }
+    },
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
